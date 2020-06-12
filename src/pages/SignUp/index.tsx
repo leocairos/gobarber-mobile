ece1bfa3668c5fb51f9717ec63ca781d7dc9140d
Leonardo Sampaio Cairo <leocairos@gmail.com>
2020-06-12 20:36:17 -0300

    List Provider

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -137,7 +137,7 @@ const SignUp: React.FC = () => {
                 }}
               />
               <Button onPress={() => formRef.current?.submitForm()}>
-                Entrar
+                Cadastrar
               </Button>
             </Form>
           </Container>
